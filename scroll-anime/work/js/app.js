20d1c6f43252e91662aa8d523c7ed82219715aba
nishi-oka <nishioka31415@gmail.com>
2024-09-22 00:06:07 +0900

    Update app.js

---
+++ scroll-anime/work/js/app.js
@@ -0,0 +1,12 @@
+const boxes = document.querySelectorAll(".box");
+const callback = (entries) => {
+	entries.forEach(entry => {
+		if(entry.isIntersecting){
+			entry.target.classList.add("displayed");
+		}else{
+		entry.target.classList.remove("displayed");
+		}
+	})
+};
+const io = new IntersectionObserver(callback);
+boxes.forEach(box => io.observe(box));
